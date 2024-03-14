9b9b7818fd014c88fb132c247257d79d3a231db6
lucas-pettinelli <pettinellilucas@gmail.com>
2024-03-14 19:52:30 -0300

    feat: benefits datas in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
-    styleUrl: './home.component.css',
-    imports: [HeaderComponent]
+    styleUrls: ['./home.component.css', './home-responsive.component.css'],
+    imports: [HeaderComponent, NgOptimizedImage]
 })
 export class HomeComponent {
 
