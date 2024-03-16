e35d86435a20971b97bac56957c8890497575074
lucas-pettinelli <pettinellilucas@gmail.com>
2024-03-16 12:45:54 -0300

    style: newsletter-form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { NgOptimizedImage } from '@angular/common';
 import { ButtonPrimaryComponent } from '../button-primary/button-primary.component';
+import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css', './home-responsive.component.css'],
-    imports: [HeaderComponent, NgOptimizedImage, ButtonPrimaryComponent]
+    imports: [HeaderComponent, NgOptimizedImage, ButtonPrimaryComponent, NewsletterFormComponent]
 })
 export class HomeComponent {
 
